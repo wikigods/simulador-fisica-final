de6f73c261f919070915f6b638e28fa8300733eb
wikigods <wikigods@gmail.com>
2025-12-14 18:54:57 -0500

    Update build output and asset organization
    Regenerated build assets, including new CSS and JS files, and moved font files from 'dist/assets' to 'dist/webfonts'. Updated 'index.html', main SCSS, and Vite config to reflect these changes.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,33 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   base: '/simulador-fisica-final/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+
+          if (/\.(woff2?|ttf|otf|eot)$/.test(assetInfo.name)) {
+            return 'webfonts/[name][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler', // o "modern"
+        silenceDeprecations: [
+          // 'mixed-decls',
+          'color-functions',
+          'global-builtin',
+          'import',
+          // 'if-function',
+        ]
+      },
+    },
+  },
 });
